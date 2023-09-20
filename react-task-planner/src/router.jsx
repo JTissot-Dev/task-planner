4133e0b4807586455c83f1c773b2453adeaa94c0
Jerome Tissot <jerome.tissot@lamache.org>
2023-09-20 20:57:05 +0200

    US1: Define React router

---
+++ react-task-planner/src/router.jsx
@@ -0,0 +1,45 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./views/login";
+import Signup from "./views/Signup";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Index from "./views/Index";
+import NotFound from "./views/NotFound";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to='/index' />
+      },
+      {
+        path: '/index',
+        element: <Index />
+      }
+    ]
+  },
+  {
+    path: '/',
+    element: <GuestLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+])
+
+export default router;
