f80edee89057df77874f62bb3ab9b1c76ee9c1a2
Jerome Tissot <jerome.tissot@lamache.org>
2023-09-21 19:05:06 +0200

    US1: Define React context

--- react-task-planner/src/components/DefaultLayout.jsx
+++ react-task-planner/src/components/DefaultLayout.jsx
@@ -1,6 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
+
 
 const DefaultLayout = () => {
+
+  const {user, token} = useStateContext();
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <div>
       <Outlet />
--- react-task-planner/src/components/GuestLayout.jsx
+++ react-task-planner/src/components/GuestLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/ContextProvider";
+
 
 const GuestLayout = () => {
+  const {user, token} = useStateContext();
+
+  if (token) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <Outlet />
